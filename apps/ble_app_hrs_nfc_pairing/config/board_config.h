b95d42c868db83db1edf51409e7c0c6c765e010c
Lu, Ken <bluewish.ken.lu@gmail.com>
2020-11-16 19:17:28 +0800

    Add SX126x PIN config in board_config.h

---
+++ apps/ble_app_hrs_nfc_pairing/config/board_config.h
@@ -0,0 +1,13 @@
+#ifndef __BOARD_CONFIG_H__
+#define __BOARD_CONFIG_H__
+
+// SX126X PIN
+#define SX126X_RESET             P0_20
+#define SX126X_MOSI              P0_11
+#define SX126X_MISO              P1_09
+#define SX126X_SCLK              P0_15
+#define SX126X_NSS               P0_17
+#define SX126X_BUSY              P0_18
+#define SX126X_DIO1              P0_02
+
+#endif // __BOARD_CONFIG_H__
