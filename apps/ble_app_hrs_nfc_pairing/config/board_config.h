6d769ef8ec7db0ea385f5f7ebe31313da8d2cfb7
Lu, Ken <bluewish.ken.lu@gmail.com>
2020-11-28 16:40:20 +0800

    Use PI_09 instead of P1_08

--- apps/ble_app_hrs_nfc_pairing/config/board_config.h
+++ apps/ble_app_hrs_nfc_pairing/config/board_config.h
@@ -5,7 +5,7 @@
 #define SX126X_RESET             P0_20
 #define SX126X_MOSI              P0_11
 #define SX126X_MISO              P0_15
-#define SX126X_SCLK              P1_08
+#define SX126X_SCLK              P1_09
 #define SX126X_NSS               P0_17
 #define SX126X_BUSY              P0_28
 #define SX126X_DIO1              P0_02
