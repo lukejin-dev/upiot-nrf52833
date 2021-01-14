8daa43e1d8587ee66ad6a534bee57fddefac13a6
Lu, Ken <bluewish.ken.lu@gmail.com>
2021-01-14 17:06:58 +0800

    Update PIN according to QIAA board

--- apps/ble_app_hrs_nfc_pairing/config/board_config.h
+++ apps/ble_app_hrs_nfc_pairing/config/board_config.h
@@ -6,17 +6,18 @@
 // SX126X PIN
 #define SX126X_RESET            P0_20
 #define SX126X_MOSI             P0_11
-#define SX126X_MISO             P0_15
 #define SX126X_SCLK             P1_09
 #define SX126X_NSS              P0_17
-#define SX126X_BUSY             P0_28
 #define SX126X_DIO1             P0_02
+
+#define SX126X_BUSY             P0_29
+#define SX126X_MISO             P0_30
+#define SI7021_SDA              P0_31
 
 #define BOARD_LED_1             P0_13
 #define BOARD_LED_2             P0_14
 #define BOARD_LED_3             P0_16
 
-#define SI7021_SDA              P0_05
 #define SI7021_SCL              P0_04
 
 #define LED_LORA_TX             0
